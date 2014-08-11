b54002140f40edfaf4fcc4e788e3cfcd8239cd36
A. Johnson <aaron.m.johnson@nrl.navy.mil>
2014-08-11 16:20:42 -0400

    fixing network modifier argument

--- pathsim.py
+++ pathsim.py
@@ -1739,6 +1739,7 @@ pathsim, and pickle it. The pickled object is input to the simulate command')
         network_state_files = pad_network_state_files(network_state_files)
         # create object that will add adversarial relays into network
         adv_insertion = network_modifiers.AdversaryInsertion(args, _testing)
+        network_modifiers = [adv_insertion]
         # create other network modification object
         if (args.other_network_modifier is not None):
             # dynamically import module and obtain reference to class
@@ -1750,8 +1751,7 @@ pathsim, and pickle it. The pickled object is input to the simulate command')
             network_modifier_class = getattr(network_modifier_module, classname)
             # create object of class
             other_network_modifier = network_modifier_class(args, _testing)
-        # create list of objects to modify network each consensus period
-        network_modifiers = [adv_insertion, other_network_modifier]
+            network_modifiers.append(other_network_modifier)
         # create iterator that applies network modifiers to nsf list
         network_states = get_network_states(network_state_files,
             network_modifiers)
