d6eb7b4fdf2ad4595761c8c4339318b9d8c81ad1
A. Johnson <aaron.m.johnson@nrl.navy.mil>
2014-08-11 04:38:39 -0400

    modifying run script to new argument style

--- run_quick_simulation.sh
+++ run_quick_simulation.sh
@@ -5,10 +5,13 @@ NSF_DIR=$BASE_DIR/out/network-state/slim/ns-$DATE_RANGE
 NUM_SAMPLES=1
 TRACEFILE=$BASE_DIR/in/traces_processed.pickle
 USERMODEL="simple=1"
-OUTPUT=0
+FORMAT="normal"
 ADV_GUARD_BW=0
 ADV_EXIT_BW=0
 ADV_TIME=0
 NUM_ADV_GUARDS=0
-pypy pathsim.py simulate $NSF_DIR $NUM_SAMPLES $TRACEFILE $USERMODEL $OUTPUT $ADV_GUARD_BW $ADV_EXIT_BW $ADV_TIME $NUM_ADV_GUARDS
-simulate [nsf dir] [# samples] [tracefile] [user model] [output] [adv guard cons bw] [adv exit cons bw] [adv time] [num adv guards] [path selection alg] [num guards] [guard expiration]
+NUM_ADV_EXITS=0
+NUM_GUARDS=3
+GUARD_EXPIRATION=60
+PATH_ALG=tor
+pypy pathsim.py simulate --nsf_dir $NSF_DIR --num_samples $NUM_SAMPLES --trace_file $TRACEFILE --user_model $USERMODEL --format $FORMAT --adv_guard_cons_bw $ADV_GUARD_BW --adv_exit_cons_bw $ADV_EXIT_BW --adv_time $ADV_TIME --num_adv_guards $NUM_ADV_GUARDS --num_adv_exits $NUM_ADV_EXITS --num_guards $NUM_GUARDS --guard_expiration $GUARD_EXPIRATION $PATH_ALG
